de955cf39ad807082205ba55dec5892ea8b98931
374937589 <374937589@qq.com>
2019-04-13 10:58:36 +0800

    Initial commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+package com.bdqn;
+
+/**
+ * Created by Administrator on 2019/4/13.
+ */
+public class Test {
+    public static void main(String [] args){
+        System.out.println("hello world");
+    }
+}
